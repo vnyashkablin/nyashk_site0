f7d89f8abf725d79b632d0257d943fdc82f5fcbf
няшка <vikazak@MacBook-Air-Vika.local>
2024-02-11 17:07:51 +0300

    Добавила одну песню

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Добро пожаловать на няшк сайт
+                </p>
+
+                <iframe
+                    style={{borderRadius: "12px"}}
+                    src="https://open.spotify.com/embed/track/2DkO9y7DB9S9nBg5u0VDoW?utm_source=generator"
+                    width="75%"
+                    height="352"
+                    frameBorder="0"
+                    allowFullScreen={true}
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
+            </header>
+        </div>
+    );
 }
 
 export default App;
