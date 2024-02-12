8e8e1e3fba0fb3a4a4d0a68673aafdf7babac2f1
няшка <vikazak@MacBook-Air-Vika.local>
2024-02-12 10:45:22 +0300

    Добавила еще пару песен, изменила цвет

--- src/App.js
+++ src/App.js
@@ -10,7 +10,27 @@ function App() {
 
                 <iframe
                     style={{borderRadius: "12px"}}
-                    src="https://open.spotify.com/embed/track/2DkO9y7DB9S9nBg5u0VDoW?utm_source=generator"
+                    src="https://open.spotify.com/embed/track/2DkO9y7DB9S9nBg5u0VDoW?utm_source=generator&theme=0"
+                    width="75%"
+                    height="352"
+                    frameBorder="0"
+                    allowFullScreen={true}
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
+                <iframe
+                    style={{borderRadius: "12px"}}
+                    src="https://open.spotify.com/embed/track/4OtVQ2ZxS7yigIjGz5yKg1?utm_source=generator&theme=0"
+                    width="75%"
+                    height="352"
+                    frameBorder="0"
+                    allowFullScreen={true}
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
+                <iframe
+                    style={{borderRadius: "12px"}}
+                    src="https://open.spotify.com/embed/track/45lXSvtDt6uKiGZIXB4LLF?utm_source=generator&theme=0"
                     width="75%"
                     height="352"
                     frameBorder="0"
