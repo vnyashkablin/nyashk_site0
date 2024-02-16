f8e6151b83ad911f320e49fa1e53c0c7239c2e88
няшка <vikazak@MacBook-Air-Vika.local>
2024-02-16 23:00:42 +0300

    Изменила надпись

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <p>
-                    Добро пожаловать на мой няшк сайтик
+                    Ку, Добро пожаловать на мой няшк сайтик
                 </p>
 
                 <iframe
